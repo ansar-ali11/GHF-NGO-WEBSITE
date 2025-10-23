149e37699222c43c347358df6e8f133c751a8ead
ansarali11 <mdansarali4411@gmail.com>
2025-10-23 11:13:44 +0530

    v2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,13 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  build: {
+    outDir: "dist", // ensures proper build output for Vercel
+  },
+  base: "./", // keeps paths relative and prevents 404 issues on reload
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
